687202bcc20dcb98c54123aa789559e164d3609e
ShripriyaR <shripriyaravi@gmail.com>
2023-06-27 17:07:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Rectangle from './Components/Rectangle/Rectangle';
+import Circle from './Components/Rectangle/Circle/Circle';
+import PriceCard from './Components/PriceCard/PriceCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PriceCard/>
+      
+      
+      
     </div>
   );
 }
